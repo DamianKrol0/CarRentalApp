a4f9e59efdcf48fc431cb1bfa3c32287a268a609
krol.d <krol.d@e80group.com>
2024-03-01 16:39:27 +0100

    init2

---
+++ CarRentalAppMVC/CarRentalAppMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CarRentalAppMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
